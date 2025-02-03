b5b47f569f28029daf65db418352db019279f515
Gabriel Nkwa <gnkwa@outlook.com>
2025-02-03 21:32:40 +0100

    new ui

--- app.py
+++ app.py
@@ -10,29 +10,46 @@ mongo = PyMongo(app)
 
 @app.route("/")
 def home():
-    return render_template("home.py")
+    return render_template("dashboard.html")
 
-@app.route("/register", methods=['GET','POST'])
-def act1():
-    nm = request.form.get("name")
-    sid = request.form.get("staff_id")
-    dir = request.form.get("directorate")
-    pspt1 = request.files.get("pspt1")
-    pspt2 = request.files.get("pspt2")
-    pspt3 = request.files.get("pspt3")
-    pspt4 = request.files.get("pspt4")
-    pspt5 = request.files.get("pspt5")
+
+@app.route("/analytics")
+def analytics():
+    return render_template("analytics.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/staff_registration")
+def staff_reg():
+    return render_template("staff_registration")
+
+
+
+
+
+# @app.route("/register", methods=['GET','POST'])
+# def act1():
+#     nm = request.form.get("name")
+#     sid = request.form.get("staff_id")
+#     dir = request.form.get("directorate")
+#     pspt1 = request.files.get("pspt1")
+#     pspt2 = request.files.get("pspt2")
+#     pspt3 = request.files.get("pspt3")
+#     pspt4 = request.files.get("pspt4")
+#     pspt5 = request.files.get("pspt5")
     
-    mongo.save_file()
-    data = {"Name": nm,"Staff ID":sid, "Directorate":dir, "Passport 1":pspt1.filename, "Passport 2":pspt2.filename, "Passport 3":pspt3.filename, "Passport 4":pspt4.filename, "Passport 5":pspt5.filename}
-    mongo.db.USERS.insert_one(data)
-    return render_template("test_play.html")
+#     mongo.save_file()
+#     data = {"Name": nm,"Staff ID":sid, "Directorate":dir, "Passport 1":pspt1.filename, "Passport 2":pspt2.filename, "Passport 3":pspt3.filename, "Passport 4":pspt4.filename, "Passport 5":pspt5.filename}
+#     mongo.db.USERS.insert_one(data)
+#     return render_template("test_play.html")
 
-@app.route("/show")
-def shh():
-    data = mongo.db.USERS.find()
-    data = list(data)
-    return render_template('report_page', info=data)
+# @app.route("/show")
+# def shh():
+#     data = mongo.db.USERS.find()
+#     data = list(data)
+#     return render_template('report_page', info=data)
 
 
 if __name__ == "__main__":
